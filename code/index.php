dba0d958e33cea292c5fa1b143758c6b2e5b5670
Miquel Correa <correa.miquel@gmail.com>
2022-12-06 16:19:15 +0100

    move functional code to code directory and leave an empty file in public directory

--- code/index.php
+++ code/index.php
@@ -1,3 +1,17 @@
 <?php
-echo "yay";
-phpinfo();
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/container.php';
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello my worldw!");
+    return $response;
+});
+
+$app->run();
