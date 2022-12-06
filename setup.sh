77678192bda18bd79a827a10cef7fcd72a560c61
Miquel Correa <correa.miquel@gmail.com>
2022-12-06 12:12:12 +0100

    fix conteiner names to ensure that script properly works always

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 docker-compose build --no-cache
 docker-compose up -d
-docker exec discount_api_php_1 sh -c "curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer"
-docker exec discount_api_php_1 composer install
+docker exec discount_api_php sh -c "curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer"
+docker exec discount_api_php composer install
