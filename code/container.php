93e8b37b8db9123ed91f2773621aade81b1c1928
Miquel Correa <correa.miquel@gmail.com>
2022-12-07 12:06:02 +0100

    define dbal connection

--- code/container.php
+++ code/container.php
@@ -1,3 +1,14 @@
 <?php
-
+use Doctrine\DBAL\Connection;
 $container = new DI\Container();
+
+$container->set(Connection::class, function($container) {
+    $connectionParams = [
+        'dbname' => 'discount_api',
+        'user' => 'MYSQL_USER',
+        'password' => 'MYSQL_PASSWORD',
+        'host' => 'mysql',
+        'driver' => 'pdo_mysql',
+    ];
+    return \Doctrine\DBAL\DriverManager::getConnection($connectionParams);
+});
