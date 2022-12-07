7abb16a360bb2178df308ff20314c77fff3f2a9d
Miquel Correa <correa.miquel@gmail.com>
2022-12-07 11:19:57 +0100

    remove sleept from setup script and replace it with an actual healthcheck on the mysql docker status

--- setup.sh
+++ setup.sh
@@ -8,7 +8,5 @@ docker exec discount_api_php composer install
 echo "copying sql files into mysql docker"
 docker cp mysql/discount_api.sql discount_api_mysql:/discount_api.sql
 docker cp mysql/sql-setup.txt discount_api_mysql:/sql-setup.txt
-echo "waiting for mysql server to be up and running"
-sleep 120
 echo "setting up database"
 docker exec discount_api_mysql sh -c "mysql -pMYSQL_ROOT_PASSWORD < sql-setup.txt > output.txt"
