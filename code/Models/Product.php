4e0f63b9ef5ac7c3d9942a9ff21632b13619e7ee
Miquel Correa <correa.miquel@gmail.com>
2022-12-07 14:01:32 +0100

    test getter methods for Product Model

--- code/Models/Product.php
+++ code/Models/Product.php
@@ -29,5 +29,26 @@ class Product
         string $name,
         string $category,
         int $price
-    ) {}
+    ) {
+        $this->sku = $sku;
+        $this->name = $name;
+        $this->category = $category;
+        $this->price = $price;
+    }
+
+    public function getSku() {
+        return $this->sku;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getCategory() {
+        return $this->category;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
 }
--- tests/TestCase/Models/ProductTest.php
+++ tests/TestCase/Models/ProductTest.php
@@ -17,4 +17,32 @@ class ProductTest extends TestCase
         $product = new Product('00001', 'nice boots', 'boots', 89000);
         $this->assertInstanceOf(Product::Class, $product);
     }
+
+    public function testGetSku(): void
+    {
+        $sku = '0123031';
+        $product = new Product($sku, 'nice boots', 'boots', 1234);
+        $this->assertEquals($sku, $product->getSku());
+    }
+
+    public function testGetName(): void
+    {
+        $name = 'product name for test';
+        $product = new Product('00001',  $name, 'boots', 12345);
+        $this->assertEquals( $name, $product->getName());
+    }
+
+    public function testGetCategory(): void
+    {
+        $category = 'category for test';
+        $product = new Product('00001', 'nice boots', $category, 12345);
+        $this->assertEquals($category, $product->getCategory());
+    }
+
+    public function testGetPrice(): void
+    {
+        $price = random_int(10,999999);
+        $product = new Product('00001', 'nice boots', 'boots', $price);
+        $this->assertEquals($price, $product->getPrice());
+    }
 }
