a8ec7e1b3a2fefbc0b2662e6ea0b0056d133e073
Miquel Correa <correa.miquel@gmail.com>
2022-12-11 00:20:49 +0100

    define route for controller

--- code/index.php
+++ code/index.php
@@ -1,17 +1,17 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+
+use Miquel\DiscountApi\Controllers\ProductsController;
 use Slim\Factory\AppFactory;
+
+error_reporting(E_ERROR);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/container.php';
 
 AppFactory::setContainer($container);
+
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello my worldw!");
-    return $response;
-});
+$app->get('/products', ProductsController::class);
 
 $app->run();
