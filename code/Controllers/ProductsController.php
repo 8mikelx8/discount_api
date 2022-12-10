2ae7e58c7e617a06b621334fec3fd6a48e5f98fa
Miquel Correa <correa.miquel@gmail.com>
2022-12-11 00:18:55 +0100

    create controller to handle get products request

---
+++ code/Controllers/ProductsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Miquel\DiscountApi\Controllers;
+
+use Miquel\DiscountApi\Services\ProductService;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class ProductsController
+{
+    protected $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+
+    public function __invoke(Request $request, Response $response)
+    {
+        $params = $this->getParams($request);
+        $products = $this->productService->getAllProducts($params['offset'],$params['category'],$params['priceLessThan']);
+        $response->getBody()->write(json_encode($products));
+        return $response
+          ->withHeader('Content-Type', 'application/json');
+    }
+
+    protected function getParams(Request $request): array
+    {
+        $queryParams = $request->getQueryParams();
+        $params =  [
+            'offset' => 0,
+            'category' => null,
+            'priceLessThan' => null
+        ];
+        if (isset($queryParams['offset'])) {
+            $params['offset'] = (int) $queryParams['offset'];
+        }
+        if (isset($queryParams['category'])) {
+            $params['category'] = $queryParams['category'];
+        }
+        if (isset($queryParams['priceLessThan']) && is_int($queryParams['priceLessThan'])) {
+            $params['priceLessThan'] = (int) $queryParams['priceLessThan'];
+        }
+        return $params;
+    }
+}
---
+++ tests/TestCase/Controllers/ProductsControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Test\TestCase\Controllers;
+
+use Miquel\DiscountApi\Controllers\ProductsController;
+use Miquel\DiscountApi\Services\ProductService;
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Psr7\Response as Response;
+use Slim\Factory\AppFactory;
+use Slim\Factory\ServerRequestCreatorFactory;
+
+class ProductTest extends TestCase
+{
+    public function testInvoke(): void
+    {
+        $service = $this->createMock(ProductService::class);
+        $service
+            ->expects(self::once())
+            ->method('getAllProducts')
+            ->with(1,'sandals',100000)
+            ->willReturn(['mockedServiceResponse']);
+        
+        $serverRequestCreator = ServerRequestCreatorFactory::create();
+        $request = $serverRequestCreator->createServerRequestFromGlobals()->withQueryParams(['offset'=> 1, 'category' => 'sandals', 'priceLessThan' => 100000]);
+        
+        $response = new Response();
+
+        $controller = new ProductsController($service);
+        $controllerResponse = $controller($request, $response);
+        $this->assertInstanceOf(Response::class, $controllerResponse);
+        $this->assertEquals(['mockedServiceResponse'], json_decode($controllerResponse->getBody()));
+    }
+}
