2941fe56c6df94fe4c5b7d0866f592192d63b597
Miquel Correa <correa.miquel@gmail.com>
2022-12-12 13:08:49 +0100

    add cors header to all responses and allow option requests

--- code/index.php
+++ code/index.php
@@ -14,4 +14,16 @@ $app = AppFactory::create();
 
 $app->get('/products', ProductsController::class);
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 $app->run();
